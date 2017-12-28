fcede2f6ed3765e61851d66960f8854f0f925bda
boujnane monssef <m-heroine@hotmail.com>
2017-12-28 22:57:52 +0000

    bootstraped client and server

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const chalk = require('chalk')
+const webpack = require('webpack')
+
+const config = require('./client/webpack.config.js')
+var compiler = webpack(config)
+compiler.run(function(err, stats) {
+  if (err || stats.hasErrors()) {
+    throw new Error(chalk.red('webpack build failed'))
+  }
+  console.log('webpack build finished')
+})
+
+// set static files route
+app.use('/static', express.static(path.resolve(__dirname, 'public')))
 
 // loads env variables into process.env
 require('dotenv').config()
@@ -19,6 +33,10 @@ if (!MONGO_DB_URI) {
 mongoose.connect(MONGO_DB_URI)
 mongoose.connection.on('error', () => {
   throw new Error(chalk.red(`unable to connect to database: ${MONGO_DB_URI}`))
+})
+
+app.get('/', (req, res) => {
+  res.sendfile(path.resolve(__dirname, 'public/index.html'))
 })
 
 app.listen(PORT, () => {
