c7b34ae2d97ba4a78ed89950809ceb633f7b62e3
boujnane monssef <m-heroine@hotmail.com>
2017-12-29 10:03:52 +0000

    add auth routes

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+const register = (req, res) => {
+  // handle user registration
+}
+
+const login = (req, res) => {
+  // handle user login
+}
+
+module.exports = {
+  register,
+  login
+}
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const authRoutes = require('./auth.routes')
+
 router.get('/', (req, res) => {
   res.sendfile(path.resolve(__dirname, '../../public/index.html'))
 })
+
+router.use('/', authRoutes)
 
 module.exports = router
