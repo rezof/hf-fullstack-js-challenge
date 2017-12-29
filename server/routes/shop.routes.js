eece7390f18cf8c84b25627c535a2eaf7fc0ee44
boujnane monssef <m-heroine@hotmail.com>
2017-12-29 16:23:23 +0000

    require auth for /shops

--- server/routes/shop.routes.js
+++ server/routes/shop.routes.js
@@ -1,8 +1,28 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const shopController = require('../controllers/shop.controller')
 
 const router = express.Router()
 
-router.get('/shops', shopController.fetchAll)
+const verifyAuth = (req, res, next) => {
+  var token =
+    req.body.token ||
+    req.query.token ||
+    req.headers['authorization'] ||
+    req.headers['x-access-token']
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
+      if (err) {
+        res.statusCode = 401
+        res.json({ errors: ['token not valid'] })
+      } else next()
+    })
+  } else {
+    res.statusCode = 401
+    res.json({ errors: ['authentification required!'] })
+  }
+}
+
+router.get('/shops', verifyAuth, shopController.fetchAll)
 
 module.exports = router
