6e84e0c4893dc67993527ea4f6e76ead85288dad
boujnane monssef <m-heroine@hotmail.com>
2017-12-31 00:16:14 +0000

    using res.sendFile instead of depricated res.sendfile

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -6,14 +6,14 @@ const authRoutes = require('./auth.routes')
 const shopRoutes = require('./shop.routes')
 
 router.get('/', (req, res) => {
-  res.sendfile(path.resolve(__dirname, '../../public/index.html'))
+  res.sendFile(path.resolve(__dirname, '../../public/index.html'))
 })
 
 router.use('/', authRoutes)
 router.use('/', shopRoutes)
 
 router.get('*', (req, res) => {
-  res.sendfile(path.resolve(__dirname, '../../public/index.html'))
+  res.sendFile(path.resolve(__dirname, '../../public/index.html'))
 })
 
 module.exports = router
