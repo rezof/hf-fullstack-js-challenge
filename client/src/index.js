81f4296607aba10b1cb69f88a19c6aea2698a974
boujnane monssef <m-heroine@hotmail.com>
2017-12-31 15:01:41 +0000

    protecting / route

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom'
 import Home from './modules/Home'
 import { Login, Register } from './modules/Auth'
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      component={props => {
+        return localStorage.getItem('token') ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }}
+    />
+  )
+}
 
 export default () => {
   return (
     <Router>
       <div>
-        <Route exact path="/" component={Home} />
+        <PrivateRoute exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Register} />
       </div>
