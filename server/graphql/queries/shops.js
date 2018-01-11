3633c70ad30663f742303bf313d7f61832c55a56
boujnane monssef <m-heroine@hotmail.com>
2018-01-11 22:46:10 +0000

    ordering shops by distance

--- server/graphql/queries/shops.js
+++ server/graphql/queries/shops.js
@@ -1,12 +1,53 @@
-const { GraphQLObjectType, GraphQLList } = require('graphql')
+const {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLFloat
+} = require('graphql')
 const shopType = require('../Types/shop')
 const shopModel = require('../../models/shop.model')
 
+const { distanceBetweenTwoPoints } = require('../../utils/index')
+
 const shopsQuery = {
   type: new GraphQLList(shopType),
-  async resolve() {
+  args: {
+    latitude: {
+      type: new GraphQLNonNull(GraphQLFloat)
+    },
+    longitude: {
+      type: new GraphQLNonNull(GraphQLFloat)
+    }
+  },
+  async resolve(_, args, context) {
+    // user coordinates
+    const userCoordinates = {
+      latitude: args.latitude,
+      longitude: args.longitude
+    }
+
+    // curried getDistance with user coords in context
+    const getDistance = distanceBetweenTwoPoints(userCoordinates)
+
+    // all shops
     const shops = await shopModel.all([])
-    return shops.splice(0, 30) // 30 shops max
+
+    return shops
+      .map(shop => {
+        const {
+          _id: id,
+          name,
+          picture,
+          location: { coordinates: [longitude, latitude] }
+        } = shop
+        const distance = getDistance({
+          longitude,
+          latitude
+        })
+        return { id, name, picture, distance }
+      })
+      .sort((a, b) => a.distance - b.distance)
+      .splice(0, 30) // 30 shops max
   }
 }
 
